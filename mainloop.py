d280f5ec3a6c0a53008de07198a78327b25a0ba6
tyler <tl276@njit.edu>
2019-12-29 03:30:28 -0500

    >= changed to > and -works-

--- mainloop.py
+++ mainloop.py
@@ -61,7 +61,7 @@ while(run):
     while(True):
         try:
             lowest=int(input("How Low would you Like to go: "))
-            if(lowest>tlength):#seems to work and allows 1 question setups
+            if(lowest>tlength):#seems to work and allows 1 question setups done
                 print("\nLowest must be LESS than your Highest\n")
                 continue
             break
