e0112b0e9872ca91377f13a83aba4050e8161b8c
tyler <tl276@njit.edu>
2019-12-28 22:25:45 -0500

    made ranpop function

--- domize.py
+++ domize.py
@@ -1,15 +1,20 @@
-import random
-#populate
-numbers=[]
-for x in range(11):
-    numbers.append(x)
-print(numbers)
-#randomize
-ran=[]
-for i in range(len(numbers)):
-    y=random.randint(0,len(numbers)-1)
-    ran.append(numbers[y])
-    numbers.pop(y)
+def ranpop():
+    #import random
+    #populate
+    numbers=[]
+    for x in range(11):
+        numbers.append(x)
     print(numbers)
-    print(ran)
+    #randomize
+    ran=[]
+    for i in range(len(numbers)):
+        y=random.randint(0,len(numbers)-1)
+        ran.append(numbers[y])
+        numbers.pop(y)
+        #print(numbers)
+        #print(ran)
+    return ran
+import random
+x=ranpop()
+print(x)
         
