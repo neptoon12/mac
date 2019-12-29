d91776132acb7cb6e2c18adec972df230395ba13
tyler <tl276@njit.edu>
2019-12-28 20:34:05 -0500

    made printer format function

--- MIkaylaM.py
+++ MIkaylaM.py
@@ -1,6 +1,18 @@
+def mprint(num1,num2):
+    print("  "+str(num1))
+    print("+ "+str(num2))
+    print("****")
+
 numbers=[0,1,2,3,4,5,6,7,8,9,10]
 Manswer=False
-Manswer=not Manswer
+testnum=3
+#Manswer=not Manswer
 print("Welcome to the TT math test")
 if (Manswer):
     print("good")
+for i in range(4):#up to 10
+    #print (str(testnum)+"  +  "+str(i))
+    mprint(testnum,i)
+    
+
+    
