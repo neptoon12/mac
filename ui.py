9641baf96e20d2c476f78ae2874894affea5e78b
tyler <tl276@njit.edu>
2019-12-28 21:21:49 -0500

    finished non number handling

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
-print("please enter a number")
-num=input("What number are you practicing: ")
-print("We are Practicing with number "+str(num))
-#test
-print(type(num))
+while(True):
+    print("please enter a number")
+    try:
+        num=int(input("What number are you practicing: "))
+    except ValueError:
+        print("That is not a number!!! please enter a valid number")
+    #print(isinstance(num,int))
