36a1882349c1caa9c0a9c462a6a1bf1a60b9e5bc
tyler <tl276@njit.edu>
2019-12-29 02:32:59 -0500

    added Subtraction and range notes

--- mainloop.py
+++ mainloop.py
@@ -3,7 +3,7 @@ import random
 def mprint(num1,num2):
     print("****")
     print("  "+str(num1))
-    print("+ "+str(num2))
+    print(str(sign)+" "+str(num2))
     print("****")
     
 def ranpop(default):
@@ -14,19 +14,23 @@ def ranpop(default):
     #randomize
     ran=[]
     for i in range(len(numbers)):
-        y=random.randint(0,len(numbers)-1)
+        y=random.randint(0,len(numbers)-1)#FOR FUTURE USE WITH LIMITING RANGE WITH HIGHER NUMBERS
         ran.append(numbers[y])
         numbers.pop(y)
         #print(numbers)
         #print(ran)
     return ran
 
-Flag=False# true for doubles # modify structure for future use
+#CFlag=True# Choice or normal flag
+
 print("***Hello Mikayla, Welcome to the TT math test***")
 print()
 run=True
 #mainloop
 while(run):
+    Flag=False# true for doubles # modify structure for future use
+    SFlag=False
+    sign="+"
     Cans=0#correct answers
     #handing non 0 loop
     print("What number are you practicing with?")
@@ -37,6 +41,10 @@ while(run):
             if (num=="D"):#for doubles #add HERE FOR FUTURE MODES
                print("You Have entered Doubles Mode")
                Flag=True
+               break
+            if (num=="S"):#for subtraction #add HERE FOR FUTURE MODES
+               print("You Have entered Subtraction Mode")
+               SFlag=True
                break
             num=int(num)
             break
@@ -58,23 +66,40 @@ while(run):
             try:
                 if(Flag):#signal nonstandard FOR DOubles
                     num=int(i)
+                if(SFlag):#signal subtraction
+                    num=int(tlength)
+                    sign="-"
                 mprint(num,i)
                 Mnum=int(input("Please enter the answer: "))
                 break
             except ValueError:
                 print("That is not a number!!! please enter a valid number")
-        #int(num)
-        if(num+i==Mnum):
-            print("Good Job Mikayla!!!!")
-            Cans=Cans+1
-            print()
-        else:
-            print("Incorrect!!!")
-            print()
-            print(str(num)+"+"+str(i)+" is: "+str(num+i))
-            print("__________")
-            time.sleep(4)
-            print()
+        #for adding
+        if(not SFlag):
+            if(num+i==Mnum):
+                print("Good Job Mikayla!!!!")
+                Cans=Cans+1
+                print()
+            else:
+                print("Incorrect!!!")
+                print()
+                print(str(num)+"+"+str(i)+" is: "+str(num+i))
+                print("__________")
+                time.sleep(4)
+                print()
+        #for subtracting
+        if(SFlag):
+            if(num-i==Mnum):
+                print("Good Job Mikayla!!!!")
+                Cans=Cans+1
+                print()
+            else:
+                print("Incorrect!!!")
+                print()
+                print(str(num)+"-"+str(i)+" is: "+str(num-i))
+                print("__________")
+                time.sleep(4)
+                print()
     print("Mikayla Completed the test!\n")
     print("You Got "+str(Cans)+" out of "+str(tlength+1)+" Correct")
     if (Cans==len(ran)):
