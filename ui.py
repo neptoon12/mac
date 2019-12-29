22e018b6ee4c8cc50962c2d5442ff98e5e553235
tyler <tl276@njit.edu>
2019-12-28 21:25:28 -0500

    finished proper non number handling

--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ while(True):
     print("please enter a number")
     try:
         num=int(input("What number are you practicing: "))
+        break
     except ValueError:
         print("That is not a number!!! please enter a valid number")
     #print(isinstance(num,int))
+
