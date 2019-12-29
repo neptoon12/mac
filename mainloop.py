b50090f9404d8e4d43bdfe0b7b83331e954e2a5c
tyler <tl276@njit.edu>
2019-12-28 21:04:41 -0500

    added main loop process breakdown and partial evaluation for non numbers

---
+++ mainloop.py
@@ -0,0 +1,17 @@
+run=True
+
+while(run):
+    print("please enter a number")
+    num=int(input("What number are you practicing: "))
+    print("We are Practicing with number "+str(num))
+    isinstance(, int)
+
+    
+#greetings
+#take in number
+#print question
+#take in answer
+#evaluate
+#increment correct number
+#loop
+#print correct
